00263cce43590085c0ed66255bbbd97995e8ce97
Matheus Santa Ana Oliveira <matheussantana3105@gmail.com>
2024-02-13 11:32:03 -0300

    Pasta base de templates e arquivos estáticos (base_templates, base_static)

---
+++ agenda/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from agenda import views
+app_name = 'agenda'
+urlpatterns = [
+    path('',views.index, name = 'index'),
+]
--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+context = {}
+def index(request):
+    return render(request, 'agenda/index.html', context= context)
