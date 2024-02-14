f4d9155ca19cef9f1fed803675ca5b5198757cbb
Matheus Santa Ana Oliveira <matheussantana3105@gmail.com>
2024-02-14 09:55:34 -0300

    Criando um ImageField e configurando MEDIA_URL e MEDIA_ROOT

--- agenda/models.py
+++ agenda/models.py
@@ -9,10 +9,10 @@ class Contact(models.Model):
     email = models.EmailField(max_length = 100)
     created_date = models.DateTimeField(default = timezone.now)
     description = models.TextField(blank = True)
+    show = models.BooleanField(default = True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     # category = models.ForeignKey()
-    # show = models.enums.Choices()
     # owner = models.ForeignKey()
-    # picture = models.ImageField()
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,9 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    BASE_DIR / 'static',
+    BASE_DIR / 'base_static',
 )
+STATIC_ROOT = BASE_DIR / 'static' #collectstatic
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
