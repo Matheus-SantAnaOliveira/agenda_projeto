678270f6817f30dbddcba3d29a989334db6f2cdd
Matheus Santa Ana Oliveira <matheussantana3105@gmail.com>
2024-02-14 11:27:44 -0300

    Script Python que gera dados aleatórios no Django com o Faker

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from agenda.models import Contact
+from agenda.models import Contact, Category
 # Register your models here.
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name','last_name', 'phone',)
+    list_display = ('id', 'first_name','last_name', 'phone','category',)
     ordering = ('-id',)
     # list_filter = ('created_date',)
     search_fields = ('id','first_name','last_name',)
@@ -11,3 +11,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = (200)
     # list_editable = ('first_name','last_name')
     list_display_links = 'id', 'first_name',
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
--- agenda/models.py
+++ agenda/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    name = models.CharField(max_length = 50)
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 # Create your models here.
 class Contact(models.Model):
@@ -11,8 +19,15 @@ class Contact(models.Model):
     description = models.TextField(blank = True)
     show = models.BooleanField(default = True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
-    # category = models.ForeignKey()
-    # owner = models.ForeignKey()
+    category = models.ForeignKey(
+        Category, 
+        on_delete = models.SET_NULL,
+        blank = True, null = True
+        )
+    owner = models.ForeignKey(
+        User,
+        on_delete = models.SET_NULL,
+        blank = True, null = True)
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
