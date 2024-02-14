d03e12f632bbc2457c07d746c38ef4749e2b87ca
Matheus Santa Ana Oliveira <matheussantana3105@gmail.com>
2024-02-14 14:28:39 -0300

    starting index

--- agenda/views/contact_views.py
+++ agenda/views/contact_views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from agenda.models import Contact
 # Create your views here.
-context = {}
 def index(request):
+    contacts = Contact.objects.all()
+    context = {'contacts':contacts}
+    
     return render(request, 'agenda/index.html', context= context)
