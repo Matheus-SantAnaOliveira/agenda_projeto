81327738a929fd0ebe85e0aa9fa3be2288b0a296
Matheus Santa Ana Oliveira <matheussantana3105@gmail.com>
2024-02-16 00:15:11 -0300

    HTML e CSS do nosso formulário create.html

--- agenda/views/contact_forms.py
+++ agenda/views/contact_forms.py
@@ -2,9 +2,23 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from agenda.models import Contact
 from django.core.paginator import Paginator
+from django import forms
+
+
 # Create your views here.
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name','last_name', 'phone',)
 
 def create(request):
-    query = request.POST.get('first_name')
-    context = {}
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(data=request.POST)
+        }
+        return render(request, 'agenda/create.html', context= context)
+    context = {
+            'form': ContactForm()
+        }
     return render(request, 'agenda/create.html', context= context)
+    
