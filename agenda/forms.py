ab7c9c4a352995c78c2c3ec3d4f0f40607167ba9
Matheus Santa Ana Oliveira <matheussantana3105@gmail.com>
2024-02-17 13:00:37 -0300

    criando widgets

--- agenda/forms.py
+++ agenda/forms.py
@@ -10,7 +10,14 @@ class ContactForm(forms.ModelForm):
                 }
             ), help_text='texto de ajuda'
         )
-        
+    
+    last_name = forms.CharField(
+        widget= forms.TextInput(
+            attrs={ 'class':'class-b',
+                    'placeholder':'Digite seu sobrenome'
+            }
+        ), help_text='Insira seu sobrenome'
+    )
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
@@ -21,9 +28,37 @@ class ContactForm(forms.ModelForm):
         #     'first_name':forms.TextInput(
         #         attrs={'class':'classe-a',
         #                'placeholder':'Digite seu nome'}),}    
+       
     def clean(self):
         cleaned_data = self.cleaned_data
-        self.add_error(
-            'first_name',
-            ValidationError('Mensagem de erro', code = 'invalid ')
-        )
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get("first_name")
+        if data == 'ABC':
+            self.add_error('first_name' ,
+                        ValidationError('Não digite ABC neste campo', 
+                                        code='invalid'))
+        return data
+    
+    def clean_last_name(self):
+        aaa = self.cleaned_data.get("last_name")
+        for i in self.cleaned_data:
+            print(i)
+            
+        if aaa == 'DEF':
+            self.add_error('last_name',
+                            ValidationError('Não digite DEF neste campo', 
+                                            code='invalid'))
+        return aaa
